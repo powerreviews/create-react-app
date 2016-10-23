a2ab2307d5fc1f6c9ec3d95095de0d7c60eed1f8
Francis Okonkwo <francis.okonkwo@powerreviews.com>
2016-10-23 01:51:42 -0500

    Add CSS Modules and SASS

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -153,6 +153,10 @@ module.exports = {
         loader: ExtractTextPlugin.extract('style', 'css?importLoaders=1&-autoprefixer!postcss')
         // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
       },
+      {
+        test: /(\.scss|\.sass)$/,
+        loader: ExtractTextPlugin.extract('style', 'css?-autoprefixer!postcss!sass')
+      },
       // JSON is not enabled by default in Webpack but both Node and Browserify
       // allow it implicitly so we also enable it.
       {
